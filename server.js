bab8c181809753dd3af79d67e95c947e6a510b2c
Matheus Oliveira <matheus.oliveeira@gmail.com>
2025-02-19 20:20:10 -0400

    share list server

---
+++ server.js
@@ -0,0 +1,27 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const port = process.env.PORT || 8080;
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(port, '0.0.0.0', () => {
+    console.log(`Listening on port ${port}`);
+});
+
